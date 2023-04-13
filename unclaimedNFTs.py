fbba50bc42679282cdbef40f3f63367d4216be0e
brunodev12 <112636008+brunodev12@users.noreply.github.com>
2023-04-13 11:43:22 -0300

    Update unclaimedNFTs.py

--- unclaimedNFTs.py
+++ unclaimedNFTs.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from mail import send_email
 
 url = os.environ.get('PRIVATE_URL')
 
@@ -9,8 +10,10 @@ def un_claimed_NFTs():
         data = requests.get(url)
         lista = data.json()['unclaimedNFTs']
 
-
         with open("unClaimedNft.json", "w") as jsonfile:
             json.dump(lista, jsonfile)
-    except KeyError:
-        print("There are no updates on unclaimed NFTs")
+        
+        return True
+    except:
+        send_email("There are no updates on unclaimed NFTs", 'No updates NFT')
+        return False
